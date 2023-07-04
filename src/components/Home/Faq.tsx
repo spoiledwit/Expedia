119e16d2450c1c740cc6240760b4911574048f7c
Ehab-24 <ehabs1775@gmail.com>
2023-07-04 20:56:23 +0500

    feat - added FAQ section

--- src/components/Home/Faq.tsx
+++ src/components/Home/Faq.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const Faq = () => {
-  return (
-    <div>Faq</div>
-  )
-}
+  const FAQs = [
+    {
+      index: 1,
+      question: "Why are you running?",
+      answer:
+        "I run because I love it. I run because I need it. I've run nearly everyday since 2005. Before that there was four years of cross country and track, and before that two years of middle school track and field. I didn't really enjoy it as a kid, and didn't treat it seriously for a lot of reasons, so I don't count those years. Just the last decade or so, because my running has been voluntary.",
+    },
+    {
+      index: 2,
+      question: "Why are you running?",
+      answer:
+        "I run because I love it. I run because I need it. I've run nearly everyday since 2005. Before that there was four years of cross country and track, and before that two years of middle school track and field. I didn't really enjoy it as a kid, and didn't treat it seriously for a lot of reasons, so I don't count those years. Just the last decade or so, because my running has been voluntary.",
+    },
+    {
+      index: 3,
+      question: "Why are you running?",
+      answer:
+        "I run because I love it. I run because I need it. I've run nearly everyday since 2005. Before that there was four years of cross country and track, and before that two years of middle school track and field. I didn't really enjoy it as a kid, and didn't treat it seriously for a lot of reasons, so I don't count those years. Just the last decade or so, because my running has been voluntary.",
+    },
+    {
+      index: 4,
+      question: "Why are you running?",
+      answer:
+        "I run because I love it. I run because I need it. I've run nearly everyday since 2005. Before that there was four years of cross country and track, and before that two years of middle school track and field. I didn't really enjoy it as a kid, and didn't treat it seriously for a lot of reasons, so I don't count those years. Just the last decade or so, because my running has been voluntary.",
+    },
+  ];
 
-export default Faq
+  const [open, setOpen] = useState(1);
+
+  return (
+    <div className="mb-20 p-4 md:px-40 md:py-20 flex w-full flex-col gap-2">
+      {FAQs.map((faq) => {
+        const isActive = open === faq.index;
+        return (
+          <div key={faq.index} onClick={() => setOpen(faq.index)} className="flex flex-col w-full cursor-pointer">
+            <div className=" w-full flex justify-between p-4 text-sky-900 text-lg font-bold tracking-wide rounded-t-md border-b-2 border-gray-200">
+              {faq.question}
+              {isActive ? (
+                <IoIosArrowDown size={20} className="text-gray-500" />
+              ) : (
+                <IoIosArrowUp size={20} className="text-gray-500" />
+              )}
+            </div>
+            {isActive && (
+              <div className=" w-full px-4 py-2 text-sm">{faq.answer}</div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Faq;
