f3674f3ab3b6f90b7074673ebc0405a60b6fd3f7
Ehab-24 <ehabs1775@gmail.com>
2023-07-05 10:51:12 +0500

    feat[50%] - added about page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes } from "react-router-dom"
 import { Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Layout from "./Layout"
+import AboutPage from "./pages/AboutPage"
 
 const App = () => {
   return (
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
         </Route>
       </Routes>
     )
