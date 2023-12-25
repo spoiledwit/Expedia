0b5d093efa4e0a102abf93dcee5c89a89225fdca
Ahmad Ashfaq <spoiledwit@gmail.com>
2023-12-25 11:44:38 +0500

    trying 2nd time adding pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,7 +112,20 @@ export default function RootLayout({
           gtag('config', 'G-5Y9K5KWPXV');
         `}
       </Script>
-      
+      <Script>
+        {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1544216096376457');
+fbq('track', 'PageView');
+`}
+      </Script>
 
       <body className={inter.className}>
         <Toaster />
